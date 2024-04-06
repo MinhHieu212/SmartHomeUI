936dd5f492ebeef28be2e6b262ced4fb97dde477
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2024-04-06 20:00:05 +0700

    tempo save

--- screens/VoiceScreen.js
+++ screens/VoiceScreen.js
@@ -6,20 +6,71 @@ import {
   Text,
   View,
   StatusBar,
-  s,
+  TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import { CloseIcon } from "../assets/Icons";
+import Voice from "@react-native-voice/voice";
+import { requestPermission } from "react-native-android-voice";
+import { request, PERMISSIONS } from "react-native-permissions";
+
+const requestMicrophonePermission = async () => {
+  try {
+    const granted = await request(PERMISSIONS.ANDROID.RECORD_AUDIO);
+    if (granted === "granted") {
+      // Permission granted, you can start the speech recognition
+      console.log("Microphone permission granted");
+    } else {
+      // Permission denied
+      console.log("Microphone permission denied");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
 const VoiceScreen = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [error, setError] = useState("");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    Voice.onSpeechError = (err) => setError(err.error);
+    Voice.onSpeechResults = (res) => setResult(res.value[0]);
+    Voice.onSpeechStart = () => console.log("start Speech");
+    Voice.onSpeechEnd = () => setIsRecording(false);
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startRecording = async () => {
+    try {
+      await requestMicrophonePermission();
+      await Voice.start("en-US");
+      setIsRecording(true);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+  const stopRecording = async () => {
+    try {
+      await Voice.stop();
+      setIsRecording(false);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-white mb-[70]">
       <StatusBar barStyle={"opaque"}></StatusBar>
       <Header name="Voice AI"></Header>
       <ScrollView>
         <View className="flex-1 px-3 pb-10 h-[75vh] items-center mt-3 justify-center">
-          <View className="absolute top-5 w-[90vw]bg-red-200">
+          <View className="absolute top-5 w-[90vw] bg-red-200">
             <ScrollView horizontal>
               <View className=" border-2 p-1 flex-row items-center rounded-2xl mx-3 border-blue-300">
                 <Text className="font-semibold text-blue-400 ">
@@ -29,7 +80,7 @@ const VoiceScreen = () => {
                   <CloseIcon></CloseIcon>
                 </View>
               </View>
-              <View className="border-2 p-1 flex-row items-center rounded-2xl  border-blue-300">
+              <View className="border-2 p-1 flex-row items-center rounded-2xl border-blue-300">
                 <Text className="font-semibold text-blue-400 ">
                   Turn on the light in the living room
                 </Text>
@@ -39,8 +90,18 @@ const VoiceScreen = () => {
               </View>
             </ScrollView>
           </View>
-
-          <Image source={require("../assets/VoiceImage.png")}></Image>
+          <TouchableOpacity
+            onPress={() => (isRecording ? stopRecording() : startRecording())}
+          >
+            <Text className="text-2xl">
+              {isRecording ? "Stop Recording" : "Start Recording"}
+            </Text>
+          </TouchableOpacity>
+          <View className="t-4 flex items-center justify-center">
+            <Text className="text-lg text-blue-300">{result}</Text>
+            <Text className="text-lg text-red-500">{error}</Text>
+          </View>
+          {/* <Image source={require("../assets/VoiceImage.png")}></Image> */}
         </View>
       </ScrollView>
     </SafeAreaView>
